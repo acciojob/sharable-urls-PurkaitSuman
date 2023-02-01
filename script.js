3bd3c00683bbffa4cf08c61415b2a75cd925565f
Suman Purkait <65341291+PurkaitSuman@users.noreply.github.com>
2023-02-01 12:24:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // your code here
+document.querySelector("#button").addEventListener("click", function(event) {
+  event.preventDefault();
+  var name = document.querySelector("#name").value;
+  var year = document.querySelector("#year").value;
+  document.querySelector("#url").innerHTML = "https://localhost:8080/?name=" + name + "&year=" + year;
+});
